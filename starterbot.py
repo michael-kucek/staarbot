4eb98517a0a0694af204bd93b45c592aa5aed244
mikekucek <mikekucek@gmail.com>
2016-06-14 09:11:21 -0500

    Update starterbot.py

--- starterbot.py
+++ starterbot.py
@@ -15,8 +15,8 @@ def csv_writer(data, path):
             writer.writerow(i)
 
 # constants
-SLACK_BOT_TOKEN = 'Why do I always forget to clear the token before I upload?'
-BOT_ID = 'U1F3MNK7F'
+# SLACK_BOT_TOKEN = 'Why do I always forget to clear the token before I upload?'
+# BOT_ID = 'U1F3MNK7F'
 AT_BOT = "<@" + BOT_ID + ">:"
 EXAMPLE_COMMAND = "do"
 UPTIME_COMMAND = "uptime"
